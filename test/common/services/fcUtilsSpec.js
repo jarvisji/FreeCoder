5ba356c79746bfe7631da46d5ad99b470d6196c9
Jarvis Ji <jarvis.ji@outlook.com>
2015-05-20 16:38:45 +0800

    Create fcDateUtils.

---
+++ test/common/services/fcUtilsSpec.js
@@ -0,0 +1,34 @@
+/**
+ * Created by Ting on 2015/5/20.
+ */
+describe('Test methods in fcUtils.', function () {
+  var fcDateUtils;
+
+  beforeEach(module('fcUtils'));
+  beforeEach(inject(function (_fcDateUtils_) {
+    fcDateUtils = _fcDateUtils_;
+  }));
+
+  it('Test fcDateUtils.getTodayTimestampRange().', function () {
+    var now = new Date().getTime();
+    var todayRange = fcDateUtils.getTodayTimestampRange();
+    var oneDayMilliseconds = 24 * 60 * 60 * 1000;
+
+    expect(now).toBeGreaterThan(todayRange.start);
+    expect(now).toBeLessThan(todayRange.end);
+    expect(todayRange.end - todayRange.start + 1).toEqual(oneDayMilliseconds);
+  });
+
+  it('Test fcDateUtils.isInToday()', function () {
+    expect(fcDateUtils.isInToday(0)).toBeFalsy();
+    expect(fcDateUtils.isInToday(undefined)).toBeFalsy();
+    expect(fcDateUtils.isInToday("")).toBeFalsy();
+    expect(fcDateUtils.isInToday("nonDateString")).toBeFalsy();
+    expect(fcDateUtils.isInToday(new Date())).toBeTruthy();
+    expect(fcDateUtils.isInToday(new Date().getTime())).toBeTruthy();
+    expect(fcDateUtils.isInToday(new Date().toString())).toBeTruthy();
+    var oneDayMilliseconds = 24 * 60 * 60 * 1000;
+    expect(fcDateUtils.isInToday(new Date().getTime() + oneDayMilliseconds)).toBeFalsy();
+    expect(fcDateUtils.isInToday(new Date().getTime() - oneDayMilliseconds)).toBeFalsy();
+  })
+});
